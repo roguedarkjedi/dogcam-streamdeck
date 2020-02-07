a5c41c918941ef03fa027b4ed74c4470c1ac043c
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2020-02-06 16:06:34 -0800

    Add files via upload
    attempting to fix spacing

--- Plugin/com.roguedarkjedi.dogcam.sdPlugin/app.js
+++ Plugin/com.roguedarkjedi.dogcam.sdPlugin/app.js
@@ -3,7 +3,7 @@
 $SD.on('connected', (jsonObj) => connected(jsonObj));
 
 function connected(jsn) {
-    $SD.on('com.roguedarkjedi.dogcam.reset.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
+	$SD.on('com.roguedarkjedi.dogcam.reset.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
 	$SD.on('com.roguedarkjedi.dogcam.up.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
 	$SD.on('com.roguedarkjedi.dogcam.down.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
 	$SD.on('com.roguedarkjedi.dogcam.left.keyDown', (jsonObj) => action.onKeyDown(jsonObj));
@@ -11,7 +11,7 @@ function connected(jsn) {
 	
 	// TODO: Make global settings
 	$SD.on('com.roguedarkjedi.dogcam.reset.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
-    $SD.on('com.roguedarkjedi.dogcam.reset.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+	$SD.on('com.roguedarkjedi.dogcam.reset.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
 	
 	$SD.on('applicationDidLaunch', (jsonObj) => action.onApplicationStarted(jsonObj));
 	$SD.on('applicationDidTerminate', (jsonObj) => action.onApplicationExit(jsonObj));
@@ -21,10 +21,11 @@ function connected(jsn) {
 /** ACTIONS */
 
 const action = {
-    settings:{},
+	settings:{},
 	websocket: null,
 	resetContext: "",
 	applicationRunning: false,
+	
 	startDogcamConnect: function() {
 		if (this.websocket != null) {
 			console.log("Connect was called but we are already connected!");
@@ -73,14 +74,14 @@ const action = {
 		setTimeout(this.exitDogcam, 15000);
 	},
 	
-    onDidReceiveSettings: function(jsn) {
-        console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
+	onDidReceiveSettings: function(jsn) {
+		console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
 
-        this.settings = Utils.getProp(jsn, 'payload.settings', {});
-    },
+		this.settings = Utils.getProp(jsn, 'payload.settings', {});
+	},
 
-    onKeyDown: function (jsn) {
-        var angularDirection = 1.0;
+	onKeyDown: function (jsn) {
+		var angularDirection = 1.0;
 		var moveType = "moverel";
 		
 		if (this.settings) {
@@ -99,62 +100,62 @@ const action = {
 
 		var actionType = jsn.action.replace("com.roguedarkjedi.dogcam.", "");
 		switch (actionType) {
-			case "up":
-				this.websocket.send('{"servo": "tilt", "action": "'+moveType+'", "angle": '+angularDirection+'}');
+		case "up":
+			this.websocket.send('{"servo": "tilt", "action": "'+moveType+'", "angle": '+angularDirection+'}');
 			break;
-			case "down":
-				this.websocket.send('{"servo": "tilt", "action": "'+moveType+'", "angle": -'+angularDirection+'}');
+		case "down":
+			this.websocket.send('{"servo": "tilt", "action": "'+moveType+'", "angle": -'+angularDirection+'}');
 			break;
-			case "left":
-				this.websocket.send('{"servo": "pan", "action": "'+moveType+'", "angle": -'+angularDirection+'}');
+		case "left":
+			this.websocket.send('{"servo": "pan", "action": "'+moveType+'", "angle": -'+angularDirection+'}');
 			break;
-			case "right":
-				this.websocket.send('{"servo": "pan", "action": "'+moveType+'", "angle": '+angularDirection+'}');
+		case "right":
+			this.websocket.send('{"servo": "pan", "action": "'+moveType+'", "angle": '+angularDirection+'}');
 			break;
-			case "reset":
-				this.websocket.send('{"servo": "tilt", "action": "resetall"}');
-				$SD.api.showOk(jsn.context);
+		case "reset":
+			this.websocket.send('{"servo": "tilt", "action": "resetall"}');
+			$SD.api.showOk(jsn.context);
 			break;
-			default:
-				console.log("Action type: "+actionType+" is not recognized!");
+		default:
+			console.log("Action type: "+actionType+" is not recognized!");
 		}
-    },
+	},
 	
 	onWillAppear: function(jsn) {
 		// Save this context value
 		this.resetContext = jsn.context;
 	},
 
-    onSendToPlugin: function (jsn) {
-        /**
-         * this is a message sent directly from the Property Inspector 
-         * (e.g. some value, which is not saved to settings) 
-         * You can send this event from Property Inspector (see there for an example)
-         */ 
+	onSendToPlugin: function (jsn) {
+		/**
+		* this is a message sent directly from the Property Inspector 
+		* (e.g. some value, which is not saved to settings) 
+		* You can send this event from Property Inspector (see there for an example)
+		*/ 
 
-        const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
-        if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-            this.startDogcamConnect();
-        }
-    },
+		const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
+		if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+			this.startDogcamConnect();
+		}
+	},
 
-    /**
-     * This snippet shows, how you could save settings persistantly to Stream Deck software
-     * It is not used in this example plugin.
-     */
+	/**
+	* This snippet shows, how you could save settings persistantly to Stream Deck software
+	* It is not used in this example plugin.
+	*/
 
-    saveSettings: function (jsn, sdpi_collection) {
-        console.log('saveSettings:', jsn);
-        if (sdpi_collection != null && sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
-            if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-                this.settings[sdpi_collection.key] = sdpi_collection.value;
-                console.log('setSettings....', this.settings);
-                $SD.api.setSettings(jsn.context, this.settings);
-            }
-        }
+	saveSettings: function (jsn, sdpi_collection) {
+		console.log('saveSettings:', jsn);
+		if (sdpi_collection != null && sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
+			if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+				this.settings[sdpi_collection.key] = sdpi_collection.value;
+				console.log('setSettings....', this.settings);
+				$SD.api.setSettings(jsn.context, this.settings);
+			}
+		}
 		else {
 			$SD.api.setSettings(jsn.context, this.settings);
 		}
-    }
+	}
 };
 
